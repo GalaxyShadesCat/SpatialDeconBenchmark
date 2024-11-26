ca7680e6ee421a4919deb0c1369eca696bc64cb7
Tptpvy <136075294+Tptpvy@users.noreply.github.com>
2024-11-26 12:16:21 +0800

    Add files via upload

---
+++ scripts/RCTD/RCTD_seqFISH+.R
@@ -0,0 +1,58 @@
+library(spacexr)
+library(Matrix)
+library(readxl)
+setwd('C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/STdeconv_benchmark/methods/RCTD/')
+
+# data loading
+st_counts_fp=("C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/STdeconv_benchmark/methods/datasets/seqFISH+/gene_expressions.csv")
+st_locations_fp="C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/STdeconv_benchmark/methods/datasets/seqFISH+/Lem_Locations2.csv"
+sc_counts_fp="C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/STdeconv_benchmark/methods/datasets/seqFISH+/raw_somatosensory_sc_exp.txt"
+sc_labels_fp="C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/STdeconv_benchmark/methods/datasets/seqFISH+/somatosensory_sc_labels.txt"
+
+st_counts=read.csv(st_counts_fp,sep=",",row.names=1) # row name is global bin id
+st_counts=t(st_counts)
+st_locations=read.csv(st_locations_fp,sep=",",row.name=4) # row name is global bin id
+st_locations=st_locations[,c("X_bin","Y_bin")]
+
+sc_counts=read.csv(sc_counts_fp,sep="\t",row.names=1)
+sc_labels=read.csv(sc_labels_fp,header=FALSE)$V1
+names(sc_labels)=colnames(sc_counts)
+
+load_seqFISH=function(){ 
+  ret_list=list(
+    st_counts=st_counts,
+    st_locations=st_locations,
+    sc_counts=sc_counts,
+    sc_labels=sc_labels
+  )
+  return(ret_list)
+}
+
+data = load_seqFISH()
+out_dir="../data" # output file path
+dir.create(out_dir,recursive = TRUE, showWarnings = FALSE)
+out_matrix_norm_fp=file.path(out_dir,sprintf("seqFISH_10000.RCTD.norm3.csv")) # output file name
+
+sc_reference=Reference(
+  counts=data$sc_counts,
+  cell_types = factor(data$sc_labels)
+)
+
+st_data=SpatialRNA(
+  counts=data$st_counts,
+  coords=data$st_locations,
+  require_int=FALSE
+)
+
+start_time <- Sys.time()
+
+myRCTD <- create.RCTD(st_data, sc_reference, max_cores = 8, CELL_MIN_INSTANCE = 1) # minimum number of instances for each cell type = 1
+myRCTD <- run.RCTD(myRCTD, doublet_mode = 'doublet') # doublet mode
+
+end_time <- Sys.time()
+
+weights=myRCTD@results$weights
+norm_weights=normalize_weights(weights) # get cell type ratios
+print(end_time-start_time)
+
+write.csv(as.matrix(norm_weights),out_matrix_norm_fp)
