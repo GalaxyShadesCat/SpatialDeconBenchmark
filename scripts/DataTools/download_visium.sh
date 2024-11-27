c652473ebdae12e068b0c02488e80e6ec93bcd82
Lem <lemhui.inbox@gmail.com>
2024-11-27 18:55:04 +0800

    Make Visium script more robust
    Signed-off-by: Lem <lemhui.inbox@gmail.com>

--- scripts/DataTools/download_visium.sh
+++ scripts/DataTools/download_visium.sh
@@ -3,22 +3,28 @@
 # Create the directory if it does not exist
 mkdir -p data/Visium/input
 
-# Download single nucleus count matrix for female mice
-# Count matrix
-wget -P data/Visium/input https://www.ebi.ac.uk/biostudies/files/E-MTAB-11115/5705STDY8058280_filtered_feature_bc_matrix.h5
-# Cell annotation
-wget -P data/Visium/input https://www.ebi.ac.uk/biostudies/files/E-MTAB-11115/cell_annotation.csv
-
-# Download spatial count matrix and coordinates for female mice
-# Feature matrix
-wget P data/Visium/input https://www.ebi.ac.uk/biostudies/files/E-MTAB-11114/ST8059048_filtered_feature_bc_matrix.h5
-# Coordinates and histology image
-wget P data/Visium/input https://www.ebi.ac.uk/biostudies/files/E-MTAB-11114/ST8059048_spatial.tar.gz
-
-# Navigate to the input directory
+# Change to the input directory
 cd data/Visium/input || exit
 
-# Extract the spatial data
-tar -xvzf ST8059048_spatial.tar.gz
+# Download single nucleus count matrix for female mice
+if [ ! -f "5705STDY8058280_filtered_feature_bc_matrix.h5" ]; then
+    wget https://www.ebi.ac.uk/biostudies/files/E-MTAB-11115/5705STDY8058280_filtered_feature_bc_matrix.h5
+fi
+if [ ! -f "cell_annotation.csv" ]; then
+    wget https://www.ebi.ac.uk/biostudies/files/E-MTAB-11115/cell_annotation.csv
+fi
+
+# Download spatial count matrix and coordinates for female mice
+if [ ! -f "ST8059048_filtered_feature_bc_matrix.h5" ]; then
+    wget https://www.ebi.ac.uk/biostudies/files/E-MTAB-11114/ST8059048_filtered_feature_bc_matrix.h5
+fi
+if [ ! -f "ST8059048_spatial.tar.gz" ]; then
+    wget https://www.ebi.ac.uk/biostudies/files/E-MTAB-11114/ST8059048_spatial.tar.gz
+fi
+
+# Extract the spatial data if not already extracted
+if [ ! -d "spatial" ]; then
+    tar -xvzf ST8059048_spatial.tar.gz
+fi
 
 echo "Download Visium dataset completed"
