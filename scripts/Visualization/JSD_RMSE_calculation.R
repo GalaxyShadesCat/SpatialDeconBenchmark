0b48ff2afd6efbd78ee6c6840e81224947447684
Tptpvy <136075294+Tptpvy@users.noreply.github.com>
2024-11-28 19:58:15 +0800

    Update JSD_RMSE_calculation.R

--- scripts/Visualization/JSD_RMSE_calculation.R
+++ scripts/Visualization/JSD_RMSE_calculation.R
@@ -1,13 +1,12 @@
 ### calculation of JSD and RMSE for seqFISH+
-setwd('C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/STdeconv_benchmark/evaluation & visualization/')
 library(stringr)
 source('metrics.R')
 
 ### seqFISH+
 
-ground_truth = read.csv('C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/seqFISH_10000_Result/cell_ratios.csv',row.names=1)
+ground_truth = read.csv('../data/seqFISH/ground_truth.csv',row.names=1)
 ground_truth = as.matrix(ground_truth)
-setwd('C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/STdeconv_benchmark/evaluation & visualization/data/')
+setwd('../results/seqFISH/')
 ls_method = list.files()
 Results = data.frame(matrix(ncol = ncol(ground_truth) +2, nrow = length(ls_method)))
 for(i in 1:length(ls_method)){
@@ -21,4 +20,4 @@ for(i in 1:length(ls_method)){
 colnames(Results) = c('JSD','total_RMSE',colnames(ground_truth))
 rownames(Results) = gsub('.csv','',ls_method)
 
-write.csv(Results,'C:/Users/Vincent Yeung/Desktop/School/BIOF3001/Project/seqFISH_10000_Result/seqFISH_10000_JSD_RMSE.csv')
+write.csv(Results,'../results/seqFISH_JSD_RMSE.csv')
