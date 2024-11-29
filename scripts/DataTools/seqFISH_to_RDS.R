08ff4bd0787a8244e43b5369886075ea195b18a4
vanyu26 <vanessayuhk@gmail.com>
2024-11-30 00:53:14 +0800

    Update seqFISH_to_RDS.R

--- scripts/DataTools/seqFISH_to_RDS.R
+++ scripts/DataTools/seqFISH_to_RDS.R
@@ -8,15 +8,14 @@ path <- paste0(current_working_directory, "/data/seqFISH/")
 setwd(path)
 
 # Format seqFISH data in RDS object
-sc_counts <- read.csv("sc_counts.csv", row.names=1, header = TRUE)
-sc_counts <- t(sc_counts)
-sc_labels <- read.csv("sc_labels.csv", header=FALSE)$V1
+sc_counts <- read.csv("raw_data/raw_somatosensory_sc_exp.txt", row.names=1, sep = "\t")
+sc_labels <- read.csv("raw_data/somatosensory_sc_labels.txt", header=FALSE)$V1
 names(sc_labels) <- colnames(sc_counts)
 st_counts <- read.csv("st_counts.csv", header = TRUE, row.names = 1)
 st_locations <- read.csv("st_coords.csv", header = TRUE, row.names = 1)
 
 data <- list(
-  st_counts=st_counts,
+  st_counts=t(st_counts),
   st_locations=st_locations,
   sc_counts=sc_counts,
   sc_labels=sc_labels
