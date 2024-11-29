f75391f30c82e5fb2a5b6211eaa1ba96b2716d4e
vanyu26 <vanessayuhk@gmail.com>
2024-11-29 20:42:48 +0800

    Add files via upload

---
+++ scripts/DataTools/convert_to_rds.R
@@ -0,0 +1,24 @@
+### Create RDS object from seqFISH+ data ###
+# Set paths for input and output
+current_working_directory <- getwd()
+
+# Set paths for input and output using the current working directory
+path <- paste0(current_working_directory, "/data/seqFISH/")
+
+setwd(path)
+
+# Format seqFISH data in RDS object
+sc_counts <- read.csv("sc_counts.txt", sep="\t",row.names=1, header = TRUE) 
+sc_labels <- read.csv("sc_labels.txt", header=FALSE)$V1
+name(sc_labels) <- colnames(sc_counts)
+st_counts <- read.csv("st_counts.txt", header = TRUE, row.names = 1)
+st_locations <- read.csv("st_coords.txt", header = TRUE, row.names = 1)
+
+data <- list(
+  st_counts=t(st_counts),
+  st_locations=st_locations,
+  sc_counts=sc_counts,
+  sc_labels=sc_labels
+)
+
+saveRDS(data, "filtered_data.rds")
